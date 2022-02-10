95c2401426f1bc7f6796279b30c5ee7c9ff56fd2
barbokupna <barbokupna@gmail.com>
2022-02-10 14:41:12 -0600

    updates

---
+++ truffle-config.js
@@ -0,0 +1,32 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+// Be sure to match this mnemonic with that in Ganache!
+var mnemonic = "riot owner kite myth space into organ taste collect gather project genuine";
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*"      // Any network (default: none)
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 10);
+      },
+      network_id: '*',
+      gas: 9999999
+    }
+  },
+  compilers: { solc: {
+
+    version: "0.4.25", // Fetch exact version from solc-bin (default: truffle's version)
+    
+
+    
+    // docker: true, // Use "0.5.1" you've installed locally with docker (default: false)
+    
+    // settings: { // See the solidity docs for advice about optimization and evmVersion // optimizer: { // enabled: false, // runs: 200 // }, // evmVersion: "byzantium" //
+   } }
+};
+
